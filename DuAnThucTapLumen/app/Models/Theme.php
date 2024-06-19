632782dae48b0223ce4324a05f0341ce04682a22
VinhNTpc <vinhntpv03157@fpt.edu.vn>
2024-06-19 15:39:11 +0700

    models and migration

---
+++ DuAnThucTapLumen/app/Models/Theme.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Theme extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'thumbnail',
+        'slug',
+        'status',
+        'file',
+        'category_id'
+    ];
+}
