a61f838ee0f3c3e07e16fef412bc0873b852061b
ngogiangnampc04380 <119033482+ngogiangnampc04380@users.noreply.github.com>
2024-06-19 17:07:54 +0700

    [SQL] - cart và cart_detail migrate, model

---
+++ DuAnThucTapLumen/app/Models/Cart_detail.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart_detail extends Model
+{
+    use  HasFactory;
+    protected $table = 'cart_details';
+
+    protected $fillable = [
+        'price', 'thumbnail','cart_id','theme_id','theme_name',
+    ];
+
+   
+}
---
+++ DuAnThucTapLumen/database/migrations/2024_06_19_081910_cart_details_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('theme_name')->comment('Tên sản phẩm');
+            $table->integer('price')->comment('Đơn giá');
+            $table->string('thumbnail')->comment('Ảnh sản phẩm');
+            $table->unsignedBigInteger('cart_id');
+            $table->foreign('cart_id')->references('id')->on('carts')->constrained()->cascadeOnDelete();
+            $table->unsignedBigInteger('theme_id');
+            $table->foreign('theme_id')->references('id')->on('themes')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
